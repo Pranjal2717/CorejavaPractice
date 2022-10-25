fe452dea736c102434e7f1e64687552f59d2bd91
hp <hp@LAPTOP-HB0RKING>
2022-10-25 16:30:09 +0530

    loop demo

---
+++ loopsDemo/src/loopsDemo/loops.java
@@ -0,0 +1,34 @@
+/*@Author: Pranjal Rajeev
+@Date: 25/10/22*/
+//Packages
+package loopsDemo;
+
+//creating class
+public class loops {
+
+	//creating main 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//declaring instance variable
+		int myVal=15;
+		
+		//calling methods
+		print();
+		value(myVal);
+		
+	}
+	
+	//method for printing
+	static void print()
+	{
+		System.out.println("Welcome");
+	}
+	
+	//method for value
+	static void value(int myVal)
+	{
+		System.out.println("Value is ="+ myVal);
+	}
+
+}
