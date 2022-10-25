c9ea42348f881161d6879f1ae7850ffe2262fbb2
hp <hp@LAPTOP-HB0RKING>
2022-10-25 17:09:13 +0530

    Calculator running fine using methods and 2 packages.

---
+++ Calculater/src/com/cal/Calculator.java
@@ -0,0 +1,31 @@
+/*@Author: Pranjal Rajeev
+@Date: 25/10/22*/
+package com.cal;
+
+//creating class for calculator
+public class Calculator
+{
+	//method for addition
+	public static float addition(float num1, float num2)
+	{
+		return num1+num2;
+	}
+	
+	//method for substraction
+	public static float substraction(float num1, float num2)
+	{
+		return num1-num2;
+	}
+	
+	//method for multiplication
+	public static float multiplication(float num1, float num2)
+	{
+		return num1*num2;
+	}
+	
+	//method for division
+	public static float division(float num1, float num2)
+	{
+		return num1/num2;
+	}
+}
